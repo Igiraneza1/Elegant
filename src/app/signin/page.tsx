5da04f4e7082200f319ba7bcd2540ccfdd8326e1
Igiraneza1 <igiranezaadeline@gmail.com>
2025-06-17 10:01:00 +0300

    commit

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,59 +1,76 @@
+"use client";
+
+import React from "react";
 import Image from "next/image";
 import chair from '../../image/chair1.jpg';
+import Link from "next/link";
 
 export default function SignIn() {
   return (
-    <div className="min-h-screen flex justify-center items-center bg-gray-100">
-      <div className="flex flex-col md:flex-row w-full max-w-7xl bg-white shadow-2xl rounded-2xl overflow-hidden">
-        
-        {/* Left Side: Image */}
-        <div className="bg-blue-50 flex flex-col justify-center items-center w-full md:w-1/2 p-12">
+    <div className="grid items-center justify-center bg-gray-200 h-screen">
+      <div className="bg-white p-6 md:p-10 grid grid-cols-1 md:grid-cols-2 items-center justify-center gap-6">
+        <div className="flex items-center justify-center">
           <Image
             src={chair}
             alt="Chair"
             width={500}
-            height={500}
-            className="rounded-lg shadow-xl object-contain"
+            height={400}
+            className="rounded"
           />
         </div>
 
-    
-        <div className="flex justify-center items-center w-full md:w-1/2 p-10 md:p-16">
-          <div className="w-full max-w-lg space-y-10">
-            <h2 className="text-2xl md:text-3xl text-gray-800 font-bold">Sign In</h2>
-            <p className="text-xl text-gray-300 md:text-2xl">
-              Don’t have an account yet?{" "}
-              <a href="/sign-up" className="text-green-500 text-xl md:text-2xl hover:underline">Sign Up</a>
-            </p>
+        <div>
+          <h1 className="text-3xl font-bold mb-4">Sign In</h1>
+          <p className="text-gray-500 mb-6">
+            Do not have an account yet?{" "}
+            <Link href="/sign-up" className="text-green-500 hover:underline">
+              Sign Up
+            </Link>
+          </p>
 
-            <form className="space-y-6">
+          <form className="mt-4">
+            <div className="mb-4">
+              <label htmlFor="email" className="block text-gray-700 mb-2">
+                Username or Email
+              </label>
               <input
                 type="text"
+                id="email"
                 placeholder="Your username or email address"
-                className="w-full px-6 py-4 text-xl md:2xl text-gray-400 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
+                className="w-full px-3 py-2 p-2 rounded outline-none focus:outline-none border border-gray-300"
               />
+            </div>
+
+            <div className="mb-4">
+              <label htmlFor="password" className="block text-gray-700 mb-2">
+                Password
+              </label>
               <input
                 type="password"
-                placeholder="Password"
-                className="w-full px-6 py-4 text-xl md:2xl border border-gray-300 rounded-xl text-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                id="password"
+                placeholder="Your password"
+                className="w-full px-3 py-2 p-2 rounded outline-none focus:outline-none border border-gray-300"
               />
-              <div className="flex flex-col sm:flex-row sm:items-center justify-between text-md gap-4">
-                <label className="flex items-center gap-2">
-                  <input type="checkbox" className="accent-blue-600 w-5 h-5" />
-                  <span className="">Remember me</span>
-                </label>
-                <a href="#" className="text-gray-800 hover:underline">Forgot password?</a>
-              </div>
-              <button
-                type="submit"
-                className="w-full py-4 text-lg font-semibold text-white bg-black rounded-xl hover:bg-gray-800 transition"
-              >
-                Sign In
-              </button>
-            </form>
-          </div>
-        </div>
+            </div>
 
+            <div className="flex items-center justify-between mb-6">
+              <label className="flex items-center gap-2">
+                <input type="checkbox" className="accent-black w-4 h-4" />
+                <span className="text-gray-700">Remember me</span>
+              </label>
+              <Link href="#" className="text-gray-700 hover:underline">
+                Forgot password?
+              </Link>
+            </div>
+
+            <button
+              type="submit"
+              className="w-full bg-black text-white py-2 px-4 rounded-md focus:outline-none hover:bg-gray-800 transition"
+            >
+              Sign In
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
