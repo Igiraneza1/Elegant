44344fffc526203c42947d9b0032b009d3e6fd41
Igiraneza1 <igiranezaadeline@gmail.com>
2025-06-23 15:02:53 +0300

    commit

--- src/app/forgot/page.tsx
+++ src/app/forgot/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import Image from "next/image";
-import chair from "../../image/chair1.jpg";
+import chair from "../../../public/image/chair1.jpg"
 
 function ForgotPassword() {
   const [emailSent, setEmailSent] = useState(false);
