c6e476fc7e83f12445419e64abf578ffd4dfed4c
Igiraneza1 <igiranezaadeline@gmail.com>
2025-07-05 17:57:08 +0300

    jacket

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,22 @@
 import type { Metadata } from "next";
-import { Inter, Poppins } from "next/font/google";
+// import { Inter, Poppins } from "next/font/google";
+
 import "./globals.css";
 import Navbar from "./navbar/page";
 // import Footer from "./footer/page";
 
 // Load Google Fonts
-const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter",
-  weight: ["400", "600", "700"],
-});
+// const inter = Inter({
+//   subsets: ["latin"],
+//   variable: "--font-inter",
+//   weight: ["400", "600", "700"],
+// });
 
-const poppins = Poppins({
-  subsets: ["latin"],
-  variable: "--font-poppins",
-  weight: ["400", "600", "700"],
-});
+// const poppins = Poppins({
+//   subsets: ["latin"],
+//   variable: "--font-poppins",
+//   weight: ["400", "600", "700"],
+// });
 
 // Metadata for <head>
 export const metadata: Metadata = {
@@ -30,7 +31,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${inter.variable} ${poppins.variable}`}>
+    <html >
       <body className="font-sans antialiased bg-white text-black">
         <Navbar />
         {children}
