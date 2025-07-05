63de1daa3e1363615201501b0fa1b925b00f5826
Igiraneza1 <igiranezaadeline@gmail.com>
2025-07-05 20:34:17 +0300

    imagee

--- src/app/headset/intro/page.tsx
+++ src/app/headset/intro/page.tsx
@@ -1,22 +1,21 @@
 import Image from "next/image";
-import background from "../../../../public/image/headset/bg.png"
+import background from "../../../../public/image/headset/bg.png";
 export default function Intro() {
   return (
     <div className="">
-      <div className="w-full min-h-screen bg-yellow-200">
-        <div className="flex flex-col-reverse md:flex-row items-center justify-between px-6 py-12 md:py-24 md:px-20 gap-10">
-          <div className="w-full md:w-1/2">
+      <div className="w-full min-h-screen bg-amber-300">
+        <div className="flex flex-col-reverse md:flex-row items-center justify-between px-6 md:py-5 md:px-10 lg:px-8 py-10 gap-10">
+          <div className="w-full h-screen flex items-center justify-center relative md:w-1/2">
             <Image
               src={background}
               alt="Music background"
-              width={500}
-              height={500}
-              className="w-full h-full"
-              
+              fill
+              className="object-cover"
             />
           </div>
+
           <div className="w-full md:w-1/2 text-center md:text-left">
-            <h1 className="text-4xl sm:text-5xl md:text-6xl font-extrabold text-black leading-tight mb-6">
+            <h1 className="text-3xl sm:text-4xl md:text-5xl font-extrabold text-black leading-tight mb-6">
               Listen to <br />
               the <span className="text-blue-500">amazing</span> <br />
               music sound.
@@ -24,7 +23,7 @@ export default function Intro() {
             <p className="text-lg text-black mb-6">
               Experience music like never before
             </p>
-            <button className=" bg-black text-white font-bold py-3 px-6 rounded-lg hover:bg-gray-800 transition">
+            <button className=" bg-black text-white font-bold py-2 px-10 rounded-lg hover:bg-gray-800 transition">
               Shop Now
             </button>
           </div>
